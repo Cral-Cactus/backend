bc173f44fa097ca146602a12be48ec79f420c2e7
Cral_Cactus <marian.pb@abv.bg>
2024-08-04 00:31:42 +0300

    Created more of events

--- src/ai/backend/common/events.py
+++ src/ai/backend/common/events.py
@@ -143,30 +143,219 @@ class AgentTerminatedEvent(GenericAgentEventArgs, AbstractEvent):
 
 
 @attrs.define(slots=True, frozen=True)
-class AgentErrorEvent(AbstractEvent):
-    name = "agent_error"
+class AgentHeartbeatEvent(AbstractEvent):
+    name = "agent_heartbeat"
 
-    message: str = attrs.field()
-    traceback: Optional[str] = attrs.field(default=None)
-    user: Optional[Any] = attrs.field(default=None)
-    context_env: Mapping[str, Any] = attrs.field(factory=dict)
-    severity: LogSeverity = attrs.field(default=LogSeverity.ERROR)
+    agent_info: Mapping[str, Any] = attrs.field()
+
+    def serialize(self) -> tuple:
+        return (self.agent_info,)
+
+    @classmethod
+    def deserialize(cls, value: tuple):
+        return cls(value[0])
+
+
+@attrs.define(slots=True, frozen=True)
+class DoAgentResourceCheckEvent(AbstractEvent):
+    name = "do_agent_resource_check"
+
+    agent_id: AgentId = attrs.field()
+
+    def serialize(self) -> tuple:
+        return (self.agent_id,)
+
+    @classmethod
+    def deserialize(cls, value: tuple):
+        return cls(
+            AgentId(value[0]),
+        )
+
+
+class KernelLifecycleEventReason(enum.StrEnum):
+    AGENT_TERMINATION = "agent-termination"
+    ALREADY_TERMINATED = "already-terminated"
+    ANOMALY_DETECTED = "anomaly-detected"
+    EXEC_TIMEOUT = "exec-timeout"
+    FAILED_TO_CREATE = "failed-to-create"
+    FAILED_TO_START = "failed-to-start"
+    FORCE_TERMINATED = "force-terminated"
+    HANG_TIMEOUT = "hang-timeout"
+    IDLE_TIMEOUT = "idle-timeout"
+    IDLE_SESSION_LIFETIME = "idle-session-lifetime"
+    IDLE_UTILIZATION = "idle-utilization"
+    KILLED_BY_EVENT = "killed-by-event"
+    SERVICE_SCALED_DOWN = "service-scaled-down"
+    NEW_CONTAINER_STARTED = "new-container-started"
+    PENDING_TIMEOUT = "pending-timeout"
+    RESTARTING = "restarting"
+    RESTART_TIMEOUT = "restart-timeout"
+    RESUMING_AGENT_OPERATION = "resuming-agent-operation"
+    SELF_TERMINATED = "self-terminated"
+    TASK_DONE = "task-done"
+    TASK_FAILED = "task-failed"
+    TASK_TIMEOUT = "task-timeout"
+    TASK_CANCELLED = "task-cancelled"
+    TASK_FINISHED = "task-finished"
+    TERMINATED_UNKNOWN_CONTAINER = "terminated-unknown-container"
+    UNKNOWN = "unknown"
+    USER_REQUESTED = "user-requested"
+    NOT_FOUND_IN_MANAGER = "not-found-in-manager"
+    CONTAINER_NOT_FOUND = "container-not-found"
+
+    @classmethod
+    def from_value(cls, value: Optional[str]) -> Optional[KernelLifecycleEventReason]:
+        if value is None:
+            return None
+        try:
+            return cls(value)
+        except ValueError:
+            pass
+        return None
+
+
+@attrs.define(slots=True, frozen=True)
+class KernelCreationEventArgs:
+    kernel_id: KernelId = attrs.field()
+    session_id: SessionId = attrs.field()
+    reason: str = attrs.field(default="")
+    creation_info: Mapping[str, Any] = attrs.field(factory=dict)
 
     def serialize(self) -> tuple:
         return (
-            self.message,
-            self.traceback,
-            self.user,
-            self.context_env,
-            self.severity.value,
+            str(self.kernel_id),
+            str(self.session_id),
+            self.reason,
+            self.creation_info,
         )
 
     @classmethod
     def deserialize(cls, value: tuple):
         return cls(
-            value[0],
+            kernel_id=KernelId(uuid.UUID(value[0])),
+            session_id=SessionId(uuid.UUID(value[1])),
+            reason=value[2],
+            creation_info=value[3],
+        )
+
+
+class KernelPreparingEvent(KernelCreationEventArgs, AbstractEvent):
+    name = "kernel_preparing"
+
+
+class KernelPullingEvent(KernelCreationEventArgs, AbstractEvent):
+    name = "kernel_pulling"
+
+
+@attrs.define(auto_attribs=True, slots=True)
+class KernelPullProgressEvent(AbstractEvent):
+    name = "kernel_pull_progress"
+    kernel_id: uuid.UUID = attrs.field()
+    current_progress: float = attrs.field()
+    total_progress: float = attrs.field()
+    message: Optional[str] = attrs.field(default=None)
+
+    def serialize(self) -> tuple:
+        return (
+            str(self.kernel_id),
+            self.current_progress,
+            self.total_progress,
+            self.message,
+        )
+
+    @classmethod
+    def deserialize(cls, value: tuple):
+        return cls(
+            uuid.UUID(value[0]),
             value[1],
             value[2],
             value[3],
-            LogSeverity(value[4]),
+        )
+
+
+class KernelCreatingEvent(KernelCreationEventArgs, AbstractEvent):
+    name = "kernel_creating"
+
+
+class KernelStartedEvent(KernelCreationEventArgs, AbstractEvent):
+    name = "kernel_started"
+
+
+class KernelCancelledEvent(KernelCreationEventArgs, AbstractEvent):
+    name = "kernel_cancelled"
+
+
+@attrs.define(slots=True, frozen=True)
+class ModelServiceStatusEventArgs:
+    kernel_id: KernelId = attrs.field()
+    session_id: SessionId = attrs.field()
+    model_name: str = attrs.field()
+    new_status: ModelServiceStatus = attrs.field()
+
+    def serialize(self) -> tuple:
+        return (
+            str(self.kernel_id),
+            str(self.session_id),
+            self.model_name,
+            self.new_status.value,
+        )
+
+    @classmethod
+    def deserialize(cls, value: tuple):
+        return cls(
+            kernel_id=KernelId(uuid.UUID(value[0])),
+            session_id=SessionId(uuid.UUID(value[1])),
+            model_name=value[2],
+            new_status=ModelServiceStatus(value[3]),
+        )
+
+
+class ModelServiceStatusEvent(ModelServiceStatusEventArgs, AbstractEvent):
+    name = "model_service_status_updated"
+
+
+@attrs.define(slots=True, frozen=True)
+class KernelTerminationEventArgs:
+    kernel_id: KernelId = attrs.field()
+    session_id: SessionId = attrs.field()
+    reason: KernelLifecycleEventReason = attrs.field(default=KernelLifecycleEventReason.UNKNOWN)
+    exit_code: int = attrs.field(default=-1)
+
+    def serialize(self) -> tuple:
+        return (
+            str(self.kernel_id),
+            str(self.session_id),
+            self.reason,
+            self.exit_code,
+        )
+
+    @classmethod
+    def deserialize(cls, value: tuple):
+        return cls(
+            KernelId(uuid.UUID(value[0])),
+            session_id=SessionId(uuid.UUID(value[1])),
+            reason=value[2],
+            exit_code=value[3],
+        )
+
+
+class KernelTerminatingEvent(KernelTerminationEventArgs, AbstractEvent):
+    name = "kernel_terminating"
+
+
+class KernelTerminatedEvent(KernelTerminationEventArgs, AbstractEvent):
+    name = "kernel_terminated"
+
+
+@attrs.define(slots=True, frozen=True)
+class SessionCreationEventArgs:
+    session_id: SessionId = attrs.field()
+    creation_id: str = attrs.field()
+    reason: KernelLifecycleEventReason = attrs.field(default=KernelLifecycleEventReason.UNKNOWN)
+
+    def serialize(self) -> tuple:
+        return (
+            str(self.session_id),
+            self.creation_id,
+            self.reason,
         )

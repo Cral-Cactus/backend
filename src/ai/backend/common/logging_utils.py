a58d17e9fa9c971c2d48beb3e8a9474ed52db5c1
Cral_Cactus <marian.pb@abv.bg>
2024-08-05 15:43:42 +0300

    Created logging utils and more

---
+++ src/ai/backend/common/logging_utils.py
@@ -0,0 +1,40 @@
+import logging
+from collections.abc import Iterable
+from typing import Any, LiteralString
+
+__all__ = (
+    "BraceMessage",
+    "BraceStyleAdapter",
+    "enforce_debug_logging",
+)
+
+
+class BraceMessage:
+    __slots__ = ("fmt", "args")
+
+    def __init__(self, fmt: LiteralString, args: tuple[Any, ...]):
+        self.fmt = fmt
+        self.args = args
+
+    def __str__(self):
+        return self.fmt.format(*self.args)
+
+
+class BraceStyleAdapter(logging.LoggerAdapter):
+    def __init__(self, logger, extra=None):
+        super().__init__(logger, extra)
+
+    def log(self, level, msg, *args, **kwargs):
+        if self.isEnabledFor(level):
+            msg, kwargs = self.process(msg, kwargs)
+            kwargs["stacklevel"] = kwargs.get("stacklevel", 1) + 1
+            self.logger._log(level, BraceMessage(msg, args), (), **kwargs)
+
+
+def enforce_debug_logging(loggers: Iterable[str]) -> None:
+    root_logger = logging.getLogger()
+    for handler in root_logger.handlers:
+        handler.setLevel(logging.DEBUG)
+    for name in loggers:
+        instance = logging.getLogger(name)
+        instance.setLevel(logging.DEBUG)

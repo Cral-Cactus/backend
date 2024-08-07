04e4f00cdf0f4652f6d65cc608f161ce8ddb5755
Cral_Cactus <marian.pb@abv.bg>
2024-08-07 13:25:15 +0300

    Worked on kernel base

--- src/ai/backend/kernel/base.py
+++ src/ai/backend/kernel/base.py
@@ -218,30 +218,218 @@ class BaseRunner(metaclass=ABCMeta):
             )
             self.intrinsic_host_ports_mapping = intrinsic_host_ports_mapping
 
-        insock_port = self.intrinsic_host_ports_mapping.get("replin", "2000")
-        outsock_port = self.intrinsic_host_ports_mapping.get("replout", "2001")
-        self.insock = self.zctx.socket(zmq.PULL)
-        self.insock.bind(f"tcp://*:{insock_port}")
-        print(f"binding to tcp://*:{insock_port}")
-        self.outsock = self.zctx.socket(zmq.PUSH)
-        self.outsock.bind(f"tcp://*:{outsock_port}")
-        print(f"binding to tcp://*:{outsock_port}")
+    async def _shutdown(self) -> None:
+        try:
+            self.insock.close()
+            log.debug("shutting down...")
+            self._run_task.cancel()
+            self._main_task.cancel()
+            await self._run_task
+            await self._main_task
+            if health_check_task := self._health_check_task:
+                health_check_task.cancel()
+                await health_check_task
+            log.debug("terminating service processes...")
+            running_procs = [*self.services_running.values()]
+            async with self._service_lock:
+                await asyncio.gather(
+                    *(terminate_and_wait(proc) for proc in running_procs),
+                    return_exceptions=True,
+                )
+                await asyncio.sleep(0.01)
+            log.debug("terminated.")
+        finally:
+            await asyncio.sleep(0.1)
+            try:
+                if self.outsock:
+                    self.outsock.close()
+                await self._shutdown_jupyter_kernel()
+            finally:
+                self._log_task.cancel()
+                await self._log_task
 
-        self.log_queue = janus.Queue()
-        self.task_queue = asyncio.Queue()
-        self.init_done = asyncio.Event()
+    async def _init_jupyter_kernel(self) -> None:
 
-        setup_logger(self.log_queue.sync_q, self.log_prefix, cmdargs.debug)
-        self._log_task = loop.create_task(self._handle_logs())
-        await asyncio.sleep(0)
+        kconfigdir = Path("/home/work/.ipython/profile_default/")
+        kconfigdir.mkdir(parents=True, exist_ok=True)
+        kconfig_file = kconfigdir / "ipython_kernel_config.py"
+        kconfig_file.write_text("c.InteractiveShellApp.matplotlib = 'inline'")
 
-        self.service_parser = ServiceParser({
-            "runtime_path": str(self.runtime_path),
-        })
-        service_def_folder = Path("/etc/backend/service-defs")
-        if service_def_folder.is_dir():
-            await self.service_parser.parse(service_def_folder)
-            log.debug("Loaded new-style service definitions.")
+        kernelspec_mgr = KernelSpecManager()
+        kernelspec_mgr.ensure_native_kernel = False
+        kspecs = kernelspec_mgr.get_all_specs()
+        for kname in kspecs:
+            if self.jupyter_kspec_name in kname:
+                log.debug("starting " + kname + " kernel...")
+                self.kernel_mgr = AsyncKernelManager(kernel_name=kname)
+                await self.kernel_mgr.start_kernel()
+                if not await self.kernel_mgr.is_alive():
+                    log.error("jupyter query mode is disabled: failed to start jupyter kernel")
+                else:
+                    self.kernel_client = self.kernel_mgr.client() 
+                    assert self.kernel_client is not None
+                    self.kernel_client.start_channels(shell=True, iopub=True, stdin=True, hb=True)
+                    try:
+                        await self.kernel_client.wait_for_ready(timeout=10)
+                    except RuntimeError:
+                        log.error("jupyter channel is not active!")
+                        self.kernel_mgr = None
+                break
+        else:
+            log.debug("jupyter query mode is not available: no jupyter kernelspec found")
+            self.kernel_mgr = None
 
-        self._main_task = loop.create_task(self.main_loop(cmdargs))
-        self._run_task = loop.create_task(self.run_tasks())
+    async def _shutdown_jupyter_kernel(self):
+        if self.kernel_mgr and await self.kernel_mgr.is_alive():
+            assert self.kernel_client is not None
+            log.info("shutting down " + self.jupyter_kspec_name + " kernel...")
+            await self.kernel_mgr.shutdown_kernel()
+            self.kernel_client.stop_channels()
+            assert not await self.kernel_mgr.is_alive(), "ipykernel failed to shutdown"
+
+    async def _handle_exception(
+        self, coro: Awaitable[TReturn], help_text: str | None = None
+    ) -> TReturn | FailureSentinel:
+        try:
+            return await coro
+        except Exception as e:
+            match e:
+                case FileNotFoundError():
+                    msg = "File not found: {!r}"
+                    if help_text:
+                        msg += f" ({help_text})"
+                    log.exception(msg, e.filename)
+                case _:
+                    msg = "Unexpected error!"
+                    if help_text:
+                        msg += f" ({help_text})"
+                    log.exception(msg)
+            return FAILURE
+
+    async def _init_with_loop(self) -> None:
+        if self.init_done is not None:
+            self.init_done.clear()
+        try:
+            ret = await self._handle_exception(
+                self.init_with_loop(),
+                "Check the image configs/labels like `ai.backend.runtime-path`",
+            )
+            if ret is FAILURE:
+                log.warning(
+                    "We are skipping the runtime-specific initialization failure, "
+                    "and the container may not work as expected."
+                )
+            await self._handle_exception(
+                init_sshd_service(self.child_env),
+                "Verify agent installation with the embedded prebuilt binaries",
+            )
+        finally:
+            if self.init_done is not None:
+                self.init_done.set()
+
+    @abstractmethod
+    async def init_with_loop(self) -> None:
+
+    async def _clean(self, clean_cmd: Optional[str]) -> None:
+        ret = 0
+        try:
+            if clean_cmd is None or clean_cmd == "":
+                return
+            elif clean_cmd == "*":
+                ret = await self.clean_heuristic()
+            else:
+                ret = await self.run_subproc(clean_cmd)
+        except Exception:
+            log.exception("unexpected error")
+            ret = -1
+        finally:
+            await asyncio.sleep(0.01)
+            payload = json.dumps({
+                "exitCode": ret,
+            }).encode("utf8")
+            await self.outsock.send_multipart([b"clean-finished", payload])
+
+    async def clean_heuristic(self) -> int:
+        return 0
+
+    async def _bootstrap(self, script_path: Path) -> None:
+        log.info("Running the user bootstrap script...")
+        ret = 0
+        try:
+            ret = await self.run_subproc(["/bin/sh", str(script_path)])
+        except Exception:
+            log.exception("unexpected error while executing the user bootstrap script")
+            ret = -1
+        finally:
+            await asyncio.sleep(0.01)
+            log.info("The user bootstrap script has exited with code {}", ret)
+
+    async def _build(self, build_cmd: Optional[str]) -> None:
+        ret = 0
+        try:
+            if build_cmd is None or build_cmd == "":
+                return
+            elif build_cmd == "*":
+                if Path("Makefile").is_file():
+                    ret = await self.run_subproc("make")
+                else:
+                    ret = await self.build_heuristic()
+            else:
+                ret = await self.run_subproc(build_cmd)
+        except Exception:
+            log.exception("unexpected error")
+            ret = -1
+        finally:
+            await asyncio.sleep(0.01)
+            self._build_success = ret == 0
+            payload = json.dumps({
+                "exitCode": ret,
+            }).encode("utf8")
+            await self.outsock.send_multipart([b"build-finished", payload])
+
+    @abstractmethod
+    async def build_heuristic(self) -> int:
+        """Process build step."""
+
+    async def _execute(self, exec_cmd: str) -> None:
+        ret = 0
+        try:
+            if exec_cmd is None or exec_cmd == "":
+                return
+            elif exec_cmd == "*":
+                ret = await self.execute_heuristic()
+            else:
+                ret = await self.run_subproc(exec_cmd, batch=True)
+        except Exception:
+            log.exception("unexpected error")
+            ret = -1
+        finally:
+            await asyncio.sleep(0.01)
+            payload = json.dumps({
+                "exitCode": ret,
+            }).encode("utf8")
+            await self.outsock.send_multipart([b"finished", payload])
+
+    @abstractmethod
+    async def execute_heuristic(self) -> int:
+
+    async def _query(self, code_text: str) -> None:
+        ret = 0
+        try:
+            ret = await self.query(code_text)
+        except Exception:
+            log.exception("unexpected error")
+            ret = -1
+        finally:
+            payload = json.dumps({
+                "exitCode": ret,
+            }).encode("utf8")
+            await self.outsock.send_multipart([b"finished", payload])
+
+    async def query(self, code_text) -> int:
+        if not hasattr(self, "kernel_mgr") or self.kernel_mgr is None:
+            log.error("query mode is disabled: failed to start jupyter kernel")
+            return 127
+
+        assert self.kernel_client is not None
+        log.debug("executing in query mode...")
